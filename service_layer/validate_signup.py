86b964d59073fd4fc23e82d20b46a8e68e56cccb
Farsan Rashid <farsan.rashid@gmail.com>
2024-05-25 11:37:25 +0600

    add payload validator for validate signup

--- service_layer/validate_signup.py
+++ service_layer/validate_signup.py
@@ -1,10 +1,14 @@
 from service_layer.exceptions import InvalidPayload
-from utils.attributes import error_missing_field
+from utils.attributes import error_missing_field, error_invalid_json
 
 
 def validate_payload(payload):
     if not all(payload):
         raise InvalidPayload(error_missing_field)
+
+    if not all(isinstance(value, str)
+               for value in payload):
+        raise InvalidPayload(error_invalid_json)
 
 
 def validate_signup(token, otp, cache):
--- tests/unit/test_validate_signup.py
+++ tests/unit/test_validate_signup.py
@@ -3,6 +3,7 @@ import json
 from django.test import Client, TestCase
 from django.urls import reverse
 
+from service_layer.initiate_signup import error_invalid_json
 from utils.attributes import (
     OTP,
     TOKEN,
@@ -37,3 +38,20 @@ class ValidateSignupTests(TestCase):
             data), content_type='application/json')
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.json(), error_missing_field)
+
+    def test_validate_signup_with_invalid_json(self):
+        data = self.data
+        # OTP is supposed to be a string
+        data[OTP] = 1234  # type: ignore
+        response = self.client.post(
+            self.url, data, content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(),  error_invalid_json)
+
+        data = self.data
+        # token is supposed to be a string
+        self.data[TOKEN] = 1233  # type: ignore
+        response = self.client.post(
+            self.url, data, content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(),  error_invalid_json)
