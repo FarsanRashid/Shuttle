37c5cd2173c194da44b00844d422a9ca1bc3ead0
Farsan Rashid <farsan.rashid@gmail.com>
2024-05-26 15:31:58 +0600

    add json decode exception handler

--- accounts/validate_signup_view.py
+++ accounts/validate_signup_view.py
@@ -11,6 +11,7 @@ from utils.attributes import (
     TOKEN,
     error_invalid_request_method,
     error_server_exception,
+    error_invalid_json,
     success_signup_verification
 )
 from utils.config import REDIS_HOST, REDIS_PORT
@@ -28,6 +29,8 @@ def validate_signup_view(request):
                 host=REDIS_HOST, port=REDIS_PORT, decode_responses=True))
 
             return JsonResponse(success_signup_verification, status=201)
+        except json.JSONDecodeError:
+            return JsonResponse(error_invalid_json, status=400)
         except validate_signup.InvalidPayload as e:
             return JsonResponse(e.args[0], status=400)
         except VerificationFailed as e:
