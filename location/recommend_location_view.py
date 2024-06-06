54def76b62b16580322275a42b6618e0ed9e94c9
Farsan Rashid <farsan.rashid@gmail.com>
2024-06-06 09:33:38 +0600

    restructure if else ladder

--- location/recommend_location_view.py
+++ location/recommend_location_view.py
@@ -10,10 +10,8 @@ def recommend_location_view(request):
     if request.method == 'GET':
         if request.GET.get('q') is None:
             return JsonResponse(error_missing_paramater, status=400)
-        else:
-            if request.user.is_authenticated is False:
-                return JsonResponse({}, status=401)
-            else:
-                return JsonResponse({}, status=200)
+        if request.user.is_authenticated is False:
+            return JsonResponse({}, status=401)
+        return JsonResponse({}, status=200)
     else:
         return JsonResponse(error_invalid_request_method, status=405)
